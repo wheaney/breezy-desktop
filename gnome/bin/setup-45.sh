9da171811307564e0735b62421de7f3261c98de3
Thor <8517915+only1thor@users.noreply.github.com>
2024-04-22 22:06:22 +0200

    add second option for user group (#18)
    Co-authored-by: only1thor <>

--- gnome/bin/setup-45.sh
+++ gnome/bin/setup-45.sh
@@ -1,10 +1,17 @@
 # require a first parameter, the user home directory
 if [ -z "$1" ]; then
-    echo "Usage: $0 <username>"
+    echo "Usage: $0 <username> [group]"
     exit 1
 fi
 user=$1
 user_home=/home/$user
+
+# Check if a second parameter is provided, if not, use the username as the group
+if [ -z "$2" ]; then
+    group=$user
+else
+    group=$2
+fi
 
 # go to the downloads directory in user_home
 pushd $user_home/Downloads
@@ -14,7 +21,7 @@ wget https://github.com/wheaney/XRLinuxDriver/releases/download/breezy-test-v2/x
 wget https://github.com/wheaney/XRLinuxDriver/releases/download/breezy-test-v2/xreal_driver_setup
 
 chmod +x xreal_driver_setup
-chown $user:$user xreal*
+chown $user:$group xreal*
 
 sudo ./xreal_driver_setup $user_home/Downloads/xrealAirLinuxDriver.tar.gz
 
@@ -27,7 +34,7 @@ sed -i 's/virtual_display/breezy_desktop/g' $user_home/.xreal_driver_config
 if [ ! -d breezy-desktop ]; then
     git clone https://github.com/wheaney/breezy-desktop.git
 
-    chown -R $user:$user breezy-desktop
+    chown -R $user:$group breezy-desktop
 
     pushd breezy-desktop
     git checkout gnome-45
@@ -44,13 +51,13 @@ extensions_dir=$user_home/.local/share/gnome-shell/extensions
 
 if [ ! -d $extensions_dir ]; then
     mkdir -p $extensions_dir
-    chown $user:$user $extensions_dir
+    chown $user:$group $extensions_dir
 fi
 
 # check if the symlink at $extensions_dir/breezydesktop@org.xronlinux already exists
 if [ ! -L $extensions_dir/breezydesktop@org.xronlinux ]; then
     ln -s $user_home/Downloads/breezy-desktop/gnome/breezydesktop@org.xronlinux $extensions_dir/breezydesktop@org.xronlinux
-    chown -R $user:$user $extensions_dir/breezydesktop@org.xronlinux
+    chown -R $user:$group $extensions_dir/breezydesktop@org.xronlinux
 fi
 
 echo "Breezy Desktop extension is installed. Please log out, log back in, \
