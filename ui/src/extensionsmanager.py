638222815ece1962cdcaacbb7175d30f1dcdddcd
wheaney <42350981+wheaney@users.noreply.github.com>
2024-05-11 12:58:56 -0700

    WIP

--- ui/src/extensionsmanager.py
+++ ui/src/extensionsmanager.py
@@ -1,6 +1,7 @@
 import pydbus
 
 BREEZY_DESKTOP_UUID = "breezydesktop@xronlinux.com"
+EXTENSION_STATE_ENABLED = 1
 
 class ExtensionsManager:
     _instance = None
@@ -14,6 +15,19 @@ class ExtensionsManager:
     def __init__(self):
         self.bus = pydbus.SessionBus()
         self.gnome_shell_extensions = self.bus.get("org.gnome.Shell.Extensions")
+        self.enabled_state_change_handler = None
+
+    def on_enabled_state_change(self, enabled_state_change_handler):
+        self.enabled_state_change_handler = enabled_state_change_handler
+        self.gnome_shell_extensions.onExtensionStateChanged = self._handle_extension_state_change
+
+    def _handle_extension_state_change(self, extension_uuid, state):
+        print(f"Extension {extension_uuid} state changed to {state}")
+        if extension_uuid == BREEZY_DESKTOP_UUID and self.enabled_state_change_handler is not None:
+            for key, value in state:
+                if key == "state":
+                    self.enabled_state_change_handler(value == EXTENSION_STATE_ENABLED)
+                    break
 
     def is_installed(self):
         return self._is_installed(BREEZY_DESKTOP_UUID)
