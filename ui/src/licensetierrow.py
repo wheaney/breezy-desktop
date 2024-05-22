5750da94213bcb7724fa79bbb6969af90002c5f5
wheaney <42350981+wheaney@users.noreply.github.com>
2024-05-21 21:05:11 -0700

    WIP

--- ui/src/licensetierrow.py
+++ ui/src/licensetierrow.py
@@ -18,7 +18,19 @@ class LicenseTierRow(Gtk.Grid):
 
     tier_name = Gtk.Template.Child()
     tier_status = Gtk.Template.Child()
-    tier_funds_needed_usd = Gtk.Template.Child()
+    tier_funds_needed = Gtk.Template.Child()
+    tier_funds_needed_monthly_label = Gtk.Template.Child()
+    tier_funds_needed_monthly_amount = Gtk.Template.Child()
+    tier_funds_needed_yearly_label = Gtk.Template.Child()
+    tier_funds_needed_yearly_amount = Gtk.Template.Child()
+    tier_funds_needed_lifetime_label = Gtk.Template.Child()
+    tier_funds_needed_lifetime_amount = Gtk.Template.Child()
+
+    funds_needed_elements = {
+        'monthly': [tier_funds_needed_monthly_label, tier_funds_needed_monthly_amount],
+        'yearly': [tier_funds_needed_yearly_label, tier_funds_needed_yearly_amount],
+        'lifetime': [tier_funds_needed_lifetime_label, tier_funds_needed_lifetime_amount],
+    }
 
     def __init__(self, tier, tier_details):
         super(Gtk.Grid, self).__init__()
@@ -36,17 +48,15 @@ class LicenseTierRow(Gtk.Grid):
             if funds_needed_in_seconds is not None and funds_needed_in_seconds > 0:
                 time_remaining = time_remaining_text(funds_needed_in_seconds) 
                 if time_remaining: details += f", {time_remaining} remaining"
+            if active_period == 'lifetime' or funds_needed_in_seconds is None:
+                self.tier_funds_needed.set_visible(False)
+
         self.tier_status.set_markup(f" - <i>{status}</i> {details}")
 
-        funds_needed_markup = ''
-        first_period = True
         for period, amount in tier_details['funds_needed_by_period'].items():
-            if not first_period:
-                funds_needed_markup += ', '
-            amount_text = f"${amount}USD" if amount > 0 else 'Already funded'
-            funds_needed_markup += f"{period}: {amount_text}"
-            first_period = False
-        self.tier_funds_needed_usd.set_markup(funds_needed_markup)
+            label_widget, amount_widget = self.funds_needed_elements[period]
+            amount_text = f"US${amount}" if amount > 0 else 'Already funded'
+            amount_widget.set_markup(amount_text)
 
 def time_remaining_text(seconds):
     if not seconds:
