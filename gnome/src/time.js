4124b40f6144b4af739f5c85aa211f83175c6624
wheaney <42350981+wheaney@users.noreply.github.com>
2024-06-18 12:53:12 -0700

    Update extension so that the XR Effect gets disabled when a device disconnects

--- gnome/src/time.js
+++ gnome/src/time.js
@@ -5,3 +5,7 @@ export function getEpochSec() {
 export function toSec(milliseconds) {
     return Math.floor(milliseconds / 1000);
 }
+
+export function isValidKeepAlive(dateSec, strictCheck = false) {
+    return Math.abs(toSec(Date.now()) - dateSec) <= (strictCheck ? 1 : 5);
+}
