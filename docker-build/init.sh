3b437f1b41e47d3d07044c34661116ba734a5c56
wheaney <42350981+wheaney@users.noreply.github.com>
2024-07-16 19:38:00 -0700

    Almost working breezy multi-arch build

--- docker-build/init.sh
+++ docker-build/init.sh
@@ -13,4 +13,3 @@ fi
 echo "Building docker image"
 docker buildx build --platform linux/amd64 -f ./docker-build/Dockerfile -t "breezy-desktop:amd64" --load .
 docker buildx build --platform linux/arm64 -f ./docker-build/Dockerfile -t "breezy-desktop:arm64" --load .
-docker buildx build --platform linux/arm/v7 -f ./docker-build/Dockerfile -t "breezy-desktop:armv7" --load .
--- docker-build/run-build.sh
+++ docker-build/run-build.sh
@@ -3,17 +3,14 @@
 set -e
 
 # Create output directories
-mkdir -p out/x86_64 out/aarch64 out/armv7
+mkdir -p out/x86_64 out/aarch64
 
 # Run containers for each architecture
 sudo rm -rf build/
-docker run --rm -t -v ./:/source -v ./out/x86_64:/out --platform linux/amd64 "breezy-desktop:amd64"
+docker run --privileged --rm -t -v ./:/source -v ./out/x86_64:/out --platform linux/amd64 "breezy-desktop:amd64"
 
 sudo rm -rf build/
-docker run --rm -t -v ./:/source -v ./out/aarch64:/out --platform linux/arm64 "breezy-desktop:arm64"
-
-sudo rm -rf build/
-docker run --rm -t -v ./:/source -v ./out/armv7:/out --platform linux/arm/v7 "breezy-desktop:armv7"
+docker run --privileged --rm -t -v ./:/source -v ./out/aarch64:/out --platform linux/arm64 "breezy-desktop:arm64"
 
 # build directory structure is all owned by root because of docker, delete it all now
 sudo rm -rf build/
