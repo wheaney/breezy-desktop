55e8c786fd59dcebeb1199087c3ea922bfab00ff
wheaney <42350981+wheaney@users.noreply.github.com>
2024-10-03 12:15:40 -0700

    Partially working AppImage build

--- ui/docker-build/run-build.sh
+++ ui/docker-build/run-build.sh
@@ -8,13 +8,13 @@ GROUP=$(id -gn $USER)
 # Run containers for each architecture
 if [[ "$1" == "x86_64" || -z "$1" ]]; then
     sudo rm -rf build/
-    docker run --rm -t -v ./:/source --platform linux/amd64 "breezy-ui:amd64"
+    docker run --rm -t -v ./:/source --platform linux/amd64 --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined "breezy-ui:amd64"
     sudo chown -R $USER:$GROUP out/
 fi
 
 if [[ "$1" == "aarch64" || -z "$1"  ]]; then
     sudo rm -rf build/
-    docker run --rm -t -v ./:/source --platform linux/arm64 "breezy-ui:arm64"
+    docker run --rm -t -v ./:/source --platform linux/arm64 --device /dev/fuse --cap-add SYS_ADMIN --security-opt apparmor:unconfined "breezy-ui:arm64"
     sudo chown -R $USER:$GROUP out/
 fi
 
--- ui/src/main.py
+++ ui/src/main.py
@@ -36,15 +36,6 @@ gi.require_version('Adw', '1')
 gi.require_version('Gio', '2.0')
 gi.require_version('GLib', '2.0')
 
-user_home = os.path.expanduser('~')
-xdg_data_home = os.environ.get('XDG_DATA_HOME') or os.path.join(user_home, '.local', 'share')      
-locale_dir = os.environ.get('LOCALE_DIR', os.path.join(xdg_data_home, 'locale'))
-
-locale.setlocale(locale.LC_ALL, locale.getdefaultlocale())
-locale.bindtextdomain('breezydesktop', locale_dir)
-gettext.bindtextdomain('breezydesktop', locale_dir)
-gettext.textdomain('breezydesktop')
-
 from gi.repository import Adw, Gtk, Gio
 from .licensedialog import LicenseDialog
 from .statemanager import StateManager
