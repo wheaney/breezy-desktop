05e4e36ddbf108906bd781458fdfbfe710605590
wheaney <42350981+wheaney@users.noreply.github.com>
2024-10-20 13:21:51 -0700

    Fix enable/disable shortcut so that widescreen gets disabled with the effect

--- gnome/src/time.js
+++ gnome/src/time.js
@@ -6,6 +6,6 @@ export function toSec(milliseconds) {
     return Math.floor(milliseconds / 1000);
 }
 
-export function isValidKeepAlive(dateSec, strictCheck = false) {
-    return Math.abs(toSec(Date.now()) - dateSec) <= (strictCheck ? 1 : 5);
+export function isValidKeepAlive(dateSec) {
+    return Math.abs(toSec(Date.now()) - dateSec) <= 1;
 }
