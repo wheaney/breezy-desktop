b383ffe2ef59477340897c0af7d269a517b01e7c
Dom Rodriguez <shymega@shymega.org.uk>
2025-03-14 20:39:15 +0000

    feat(nix): Add Nix Flake to Breezy
    This derives from @johnrizzo1's Nix packages for the Nix Flake, which were
    REALLY helpful for me.
    
    Co-authored-by: John Rizzo <johnrizzo1@gmail.com>

---
+++ .nix/packages/breezy-ui/default.nix
@@ -0,0 +1,90 @@
+{
+  self,
+  version,
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  gettext,
+  glib,
+  gtk4,
+  libadwaita,
+  desktop-file-utils,
+  gobject-introspection,
+  wrapGAppsHook4,
+  python3Packages,
+  python3,
+  gst_all_1,
+  appstream,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "breezy-desktop-ui";
+  inherit version;
+
+  # Use the ui/ subtree but need root for VERSION file
+  src = self;
+  sourceRoot = "ui";
+
+  # The source is a nix store path, not a directory name
+  unpackPhase = ''
+    cp -r $src $TMPDIR/breezy-desktop
+    chmod -R u+w $TMPDIR/breezy-desktop
+    cd $TMPDIR/breezy-desktop/ui
+    sourceRoot=$(pwd)
+  '';
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    gettext
+    glib # glib-compile-schemas
+    gtk4 # gtk-update-icon-cache
+    desktop-file-utils
+    gobject-introspection
+    wrapGAppsHook4
+    appstream # appstreamcli for validation
+  ];
+
+  buildInputs = [
+    gtk4
+    libadwaita
+    glib
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    (python3.withPackages (ps:
+      with ps; [
+        pygobject3
+        pydbus
+      ]))
+  ];
+
+  # Patch the entry scripts to use Nix store paths instead of XDG_DATA_HOME
+  postPatch = ''
+    substituteInPlace src/breezydesktop.in \
+      --replace-fail "appdir = os.getenv('APPDIR', xdg_data_home)" \
+                     "appdir = os.getenv('APPDIR', '$out/share')"
+    substituteInPlace src/virtualdisplay.in \
+      --replace-fail "appdir = os.getenv('APPDIR', xdg_data_home)" \
+                     "appdir = os.getenv('APPDIR', '$out/share')" || true
+
+    # Upstream bug: virtualdisplayrow.py is missing from meson.build install list
+    substituteInPlace src/meson.build \
+      --replace-fail "'virtualdisplay.py'," "'virtualdisplay.py', 'virtualdisplayrow.py',"
+  '';
+
+  # Skip tests that need network or display
+  mesonFlags = [
+  ];
+
+  # wrapGAppsHook4 handles GI_TYPELIB_PATH, GDK_PIXBUF, etc.
+
+  meta = {
+    description = "GTK4 settings UI for Breezy Desktop XR";
+    homepage = "https://github.com/wheaney/breezy-desktop";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
+  };
+})
---
+++ .nix/packages/breezy-vulkan/default.nix
@@ -0,0 +1,58 @@
+{
+  self,
+  version,
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  meson,
+  ninja,
+  pkg-config,
+  glslang,
+  spirv-headers,
+  vulkan-headers,
+  vulkan-loader,
+  libx11,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "breezy-vulkan";
+  inherit version;
+
+  src = "${self}/vulkan/modules/vkBasalt";
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    glslang
+  ];
+
+  buildInputs = [
+    spirv-headers
+    vulkan-headers
+    vulkan-loader
+    libx11
+  ];
+
+  postInstall = let
+    sombreroSrc = "${self}/modules/sombrero";
+    vulkanSrc = "${self}/vulkan";
+  in ''
+    # Install sombrero shaders
+    mkdir -p $out/share/breezy-vulkan/shaders
+    cp ${sombreroSrc}/Sombrero.frag $out/share/breezy-vulkan/shaders/
+    cp ${sombreroSrc}/calibrating.png $out/share/breezy-vulkan/shaders/ || true
+
+    # Install breezy vulkan config
+    install -Dm644 ${vulkanSrc}/config/vkBasalt.conf $out/share/breezy-vulkan/vkBasalt.conf
+
+    # Install custom banner
+    cp ${vulkanSrc}/custom_banner.png $out/share/breezy-vulkan/ || true
+  '';
+
+  meta = {
+    description = "Vulkan post-processing layer for Breezy Desktop XR gaming";
+    homepage = "https://github.com/wheaney/breezy-desktop";
+    license = lib.licenses.gpl3Only;
+    platforms = ["x86_64-linux"];
+  };
+})
