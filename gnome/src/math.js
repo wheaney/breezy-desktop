a2eb0023dcaa30eab3f93088a7948abec534f3df
Wayne Heaney <42350981+wheaney@users.noreply.github.com>
2025-03-16 14:15:04 -0700

    Add curved display support, v2.0.15 (#103)
    * Add curved display support
    * Add GNOME 48 support
    * Update backport build and setup process, add a GNOME-45 backport build

--- gnome/src/math.js
+++ gnome/src/math.js
@@ -18,3 +18,48 @@ export function diagonalToCrossFOVs(diagonalFOVRadians, aspectRatio) {
         vertical: 2 * Math.atan(Math.tan(flatVerticalFOV / 2))
     }
 }
+
+const segmentsPerRadian = 20.0 / degreeToRadian(90.0);
+
+// displays are placed around a circle, these functions help determine radians and distances from the original
+// FOV measurements scaled to the display dimensions
+export const fovConversionFns = {
+    // convert curved FOV for flat displays
+    flat: {
+        // distance to an edge is the hypothenuse of the triangle where the opposite side is half the width of the reference fov screen
+        centerToFovEdgeDistance: (centerDistance, fovLength) => Math.sqrt(Math.pow(fovLength / 2, 2) + Math.pow(centerDistance, 2)),
+        fovEdgeToScreenCenterDistance: (edgeDistance, screenLength) => Math.sqrt(Math.pow(edgeDistance, 2) - Math.pow(screenLength / 2, 2)),
+        lengthToRadians: (fovRadians, fovLength, screenEdgeDistance, toLength) => Math.asin(toLength / 2 / screenEdgeDistance) * 2,
+        angleToLength: (fovRadians, fovLength, screenDistance, toAngleOpposite, toAngleAdjacent) => {
+            return toAngleOpposite / toAngleAdjacent * screenDistance
+        },
+        radiansToSegments: (screenRadians) => 1
+    },
+
+    // convert curved FOV for curved displays, scaling either involves no change or is linear
+    curved: {
+        centerToFovEdgeDistance: (centerDistance, fovLength) => centerDistance,
+        fovEdgeToScreenCenterDistance: (edgeDistance, screenLength) => edgeDistance,
+        lengthToRadians: (fovRadians, fovLength, screenEdgeDistance, toLength) => fovRadians / fovLength * toLength,
+        angleToLength: (fovRadians, fovLength, screenDistance, toAngleOpposite, toAngleAdjacent) => fovLength / fovRadians * Math.atan2(toAngleOpposite, toAngleAdjacent),
+        radiansToSegments: (screenRadians) => Math.ceil(screenRadians * segmentsPerRadian)
+    }
+}
+
+export const applyQuaternionToVector = (vector, quaternion) => {
+    const t = [
+        2.0 * (quaternion[1] * vector[2] - quaternion[2] * vector[1]),
+        2.0 * (quaternion[2] * vector[0] - quaternion[0] * vector[2]),
+        2.0 * (quaternion[0] * vector[1] - quaternion[1] * vector[0])
+    ];
+    return [
+        vector[0] + quaternion[3] * t[0] + quaternion[1] * t[2] - quaternion[2] * t[1],
+        vector[1] + quaternion[3] * t[1] + quaternion[2] * t[0] - quaternion[0] * t[2],
+        vector[2] + quaternion[3] * t[2] + quaternion[0] * t[1] - quaternion[1] * t[0]
+    ];
+}
+
+export const normalizeVector = (vector) => {
+    const length = Math.sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
+    return [vector[0] / length, vector[1] / length, vector[2] / length];
+}
